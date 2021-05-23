88ba6d8e6b0668a8ec0f20fc59ea1f3ceb061cf1
Ian <ianguuima@gmail.com>
2021-05-23 14:22:06 -0300

    Added Singleton Pattern

---
+++ Singleton/Example.java
@@ -0,0 +1,23 @@
+package me.ianguuima.singleton;
+
+class Database {
+
+    // a classe deve ser única, portanto, ninguém pode instancia-la
+    private Database() {}
+
+    public String performQuery(String query) {
+        // Imagine uma ação pesada no banco de dados
+        // Uma lógica de queue ou cache seria interessante aqui!
+        return query;
+    }
+
+    private static Database database;
+
+    public static Database getInstance() {
+        if (database == null) {
+            database = new Database();
+        }
+        return database;
+    }
+
+}

b220dab2d09fba20fd78b6c61ee702031f8e5a19
Ian <ianguuima@gmail.com>
2021-05-23 12:40:03 -0300

    Added example for Strategy Pattern

---
+++ Strategy/Example.java
@@ -0,0 +1,58 @@
+package me.ianguuima.strategy;
+
+interface WriteStrategy {
+    void write();
+}
+
+class WriteContext {
+    private WriteStrategy strategy;
+
+    public void write() {
+        strategy.write();
+    }
+
+    public void setStrategy(WriteStrategy strategy) {
+        this.strategy = strategy;
+    }
+}
+
+class PenStrategy implements WriteStrategy {
+    @Override
+    public void write() {
+        openSlip();
+        System.out.println("Starting to write");
+    }
+
+    // ponta da caneta
+    private void openSlip() {
+        System.out.println("Opening slip of the pen");
+    }
+}
+
+class PencilStrategy implements WriteStrategy {
+    @Override
+    public void write() {
+        sharpPencil();
+        System.out.println("Starting to write");
+    }
+
+    // Apontar o lápis
+    private void sharpPencil() {
+        System.out.println("Sharping the pencil before use");
+    }
+}
+
+
+class WriteMain {
+
+    public static void main(String[] args) {
+        WriteContext writeContext = new WriteContext();
+
+        writeContext.setStrategy(new PencilStrategy());
+        writeContext.write();
+
+        writeContext.setStrategy(new PenStrategy());
+        writeContext.write();
+    }
+
+}
